4c6cd4fffd6b7426aecaf3c7fc5b2aad5364f016
nadunmA <Naduncode404@outlook.com>
2025-12-21 11:45:00 +0530

    initial commit backend

---
+++ backend/analyzer/src/test/java/com/aeo/analyzer/AnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeo.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
