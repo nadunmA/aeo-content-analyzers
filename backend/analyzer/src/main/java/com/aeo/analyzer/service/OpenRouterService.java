fd0f33b77dbb64295a9b9db2ceb2f83d7b2206a5
nadunmA <Naduncode404@outlook.com>
2025-12-24 09:37:24 +0530

    add OpenRouterService class for AI content analysis and implement API interaction

--- backend/analyzer/src/main/java/com/aeo/analyzer/service/OpenRouterService.java
+++ backend/analyzer/src/main/java/com/aeo/analyzer/service/OpenRouterService.java
@@ -1,4 +1,68 @@
 package com.aeo.analyzer.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class OpenRouterService {
+
+    private static final Logger log = LoggerFactory.getLogger(OpenRouterService.class);
+
+    @Value("${openrouter.api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    private final String API_URL = "https://openrouter.ai/api/v1/chat/completions";
+
+    public OpenRouterService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
+
+    //Request Ai models
+    public String analyzeContent(String text, String modelName) throws Exception {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + apiKey);
+        headers.set("HTTP-Referer", "https://aeo-analyzer.com");
+        headers.set("X-Title", "AEO Analyzer");
+
+        //Text size limit
+        String limitedText = text.substring(0, Math.min(text.length(), 15000));
+
+        String prompt = "You are an AEO expert. Analyze this content and return JSON only: " + limitedText;
+
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("model", modelName);
+        requestBody.put("messages", (
+                Map.of("role", "user", "content", prompt)
+        ));
+
+        //setting to froce json
+        requestBody.put("response_format", Map.of("type", "json_object"));
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+
+        log.info("Calling OpenRouter Model: {}", modelName);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(API_URL, entity, String.class);
+
+        JsonNode root = objectMapper.readTree(response.getBody());
+        String content = root.path("choices").path(0).path("message").path("content").asText();
+
+        return content;
+
+    }
+
+
 }
