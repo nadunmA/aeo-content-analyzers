a746c9e448fa1b18af87557b6432edd8bfdbf718
nadunmA <Naduncode404@outlook.com>
2026-01-05 23:55:40 +0530

    fix: update Ubuntu AMI filter and add required filters in S3 lifecycle configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,11 +18,16 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
